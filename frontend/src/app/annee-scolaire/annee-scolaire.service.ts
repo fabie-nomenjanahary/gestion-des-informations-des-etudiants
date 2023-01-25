0b809f39cc419ee34a312485cddcd259ab2007d8
honey <fabienomenjanahary@gmail.com>
2023-01-25 10:02:13 +0100

    setting up service crud for annee-scolaire

---
+++ frontend/src/app/annee-scolaire/annee-scolaire.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { AnneeScolaire } from './annee-scolaire.model';
+const anneeScolaireURL = 'http://localhost:8080/api/annee-scolaires/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnneeScolaireService {
+  httpOptions = {
+     headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+     })
+  }
+  constructor(private http: HttpClient) { }
+
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+
+  getAll(): Observable<AnneeScolaire[]>{
+    return this.http.get<AnneeScolaire[]>(anneeScolaireURL)
+      .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  find(id: number): Observable<AnneeScolaire>{
+    return this.http.get<AnneeScolaire>(anneeScolaireURL + id)
+      .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  create(anneeScolaire: AnneeScolaire):Observable<AnneeScolaire> {
+    return this.http.post<AnneeScolaire>(anneeScolaireURL, JSON.stringify(anneeScolaire), this.httpOptions)
+      .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  update(id: number, anneeScolaire: AnneeScolaire): Observable<AnneeScolaire>{
+    return this.http.put<AnneeScolaire>(anneeScolaireURL + id, JSON.stringify(anneeScolaire), this.httpOptions)
+      .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  delete(id: number) {
+    return this.http.delete<AnneeScolaire>(anneeScolaireURL + id, this.httpOptions)
+      .pipe(
+      catchError(this.errorHandler)
+    )
+  }
+}
