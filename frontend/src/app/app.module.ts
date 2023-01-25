59e72fc7201f7a1b9b9f163a8d6e11fd915c498a
honey <fabienomenjanahary@gmail.com>
2023-01-25 08:26:32 +0100

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
