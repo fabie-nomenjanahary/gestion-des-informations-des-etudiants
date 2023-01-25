f68175fc3e98d607c84fcbb7d1badcf4439ec4c5
honey <fabienomenjanahary@gmail.com>
2023-01-25 09:28:03 +0100

    defining routes for annee scolaire

--- frontend/src/app/annee-scolaire/annee-scolaire-routing.module.ts
+++ frontend/src/app/annee-scolaire/annee-scolaire-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnneeScolaireDialogComponent } from './components/annee-scolaire-dialog/annee-scolaire-dialog.component';
+import { AnneeScolaireListComponent } from './components/annee-scolaire-list/annee-scolaire-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'personnes', redirectTo: 'personnes/list', pathMatch: 'full' },
+  { path: 'personnes/list', component: AnneeScolaireListComponent },
+  {path:'personnes/create',component:AnneeScolaireDialogComponent},
+  { path: 'personnes/edit/:id', component: AnneeScolaireDialogComponent },
+  {path:'personnes/details/:id',component:AnneeScolaireDialogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
