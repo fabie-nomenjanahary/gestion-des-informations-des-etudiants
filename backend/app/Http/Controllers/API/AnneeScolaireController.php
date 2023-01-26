631b726e2a9a644fcf2bb0151404337b82a37cc7
honey <fabienomenjanahary@gmail.com>
2023-01-26 05:32:00 +0100

    creating controller for annee-scolaire and dunctions for crud operation on it

---
+++ backend/app/Http/Controllers/API/AnneeScolaireController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\AnneeScolaire;
+use Illuminate\Http\Request;
+
+class AnneeScolaireController extends Controller
+{
+    public function getAll()
+    {
+        $data = AnneeScolaire::get();
+
+        return response()->json($data, 200);
+    }
+
+    public function create(Request $request)
+    {
+        $data['annee'] = $request['annee'];
+        $data['debutAS'] = $request['debutAS'];
+        $data['finAS'] = $request['finAS'];
+
+        AnneeScolaire::create($data);
+
+        return response()->json([
+            'message' => "Année scolaire ajoutée avec succès",
+            'success' => true
+        ], 200);
+    }
+
+    public function get($id)
+    {
+        $data = AnneeScolaire::find($id);
+
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data['annee'] = $request['annee'];
+        $data['debutAS'] = $request['debutAS'];
+        $data['finAS'] = $request['finAS'];
+
+        AnneeScolaire::find($id)->update($data);
+
+        return response()->json([
+            'message' => 'Année scolaire modifiée avec succès',
+            'success' => true
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        AnneeScolaire::find($id)->delete();
+
+        return response()->json([
+            'message' => 'Année scolaire supprimée avec succès',
+            'success' => true
+        ], 200);
+    }
+}
