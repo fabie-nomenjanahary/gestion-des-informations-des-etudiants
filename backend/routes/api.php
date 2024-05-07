59bfc95a9c0d07420a30fe9a8e9f90b2429ecbb5
Fabie Nomenjanahary <fabienomenjanahary@gmail.com>
2024-05-07 13:18:39 +0300

    created routes for new functionnalities

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,13 @@
 
 use App\Http\Controllers\API\AnneeScolaireController;
 use App\Http\Controllers\API\EtudiantController;
+use App\Http\Controllers\API\EnseignantController;
 use App\Http\Controllers\API\NiveauController;
 use App\Http\Controllers\API\ParcourController;
 use App\Http\Controllers\API\PersonneController;
+use App\Http\Controllers\API\NoteController;
+use App\Http\Controllers\API\UniteEnseignementController;
+use App\Http\Controllers\API\SemestreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -39,12 +43,42 @@ Route::prefix('etudiants')->group(function () {
     Route::delete('/{id}', [EtudiantController::class, 'delete']);
 });
 
+Route::prefix('notes')->group(function () {
+    Route::get('/', [NoteController::class, 'getAll']);
+    Route::get('/{id}', [NoteController::class, 'get']);
+    Route::post('/', [NoteController::class, 'create']);
+    Route::put('/{id}', [NoteController::class, 'update']);
+    Route::delete('/{id}', [NoteController::class, 'delete']);
+});
+
+Route::prefix('unite-enseignements')->group(function () {
+    Route::get('/', [UniteEnseignementController::class, 'getAll']);
+    Route::get('/{id}', [UniteEnseignementController::class, 'get']);
+    Route::post('/', [UniteEnseignementController::class, 'create']);
+    Route::put('/{id}', [UniteEnseignementController::class, 'update']);
+    Route::delete('/{id}', [UniteEnseignementController::class, 'delete']);
+});
+
+Route::prefix('enseignants')->group(function () {
+    Route::get('/', [EnseignantController::class, 'getAll']);
+    Route::get('/{id}', [EnseignantController::class, 'get']);
+    Route::post('/', [EnseignantController::class, 'create']);
+    Route::put('/{id}', [EnseignantController::class, 'update']);
+    Route::delete('/{id}', [EnseignantController::class, 'delete']);
+});
 Route::prefix('niveaux')->group(function () {
     Route::get('/', [NiveauController::class, 'getAll']);
     Route::get('/{id}', [NiveauController::class, 'get']);
     Route::post('/', [NiveauController::class, 'create']);
     Route::put('/{id}', [NiveauController::class, 'update']);
     Route::delete('/{id}', [NiveauController::class, 'delete']);
+
+    // semestres
+    Route::get('/semestres/liste', [SemestreController::class, 'getAll']);
+    Route::get('/semestres/{id}', [SemestreController::class, 'get']);
+    Route::post('/semestres', [SemestreController::class, 'create']);
+    Route::put('/semestres/{id}', [SemestreController::class, 'update']);
+    Route::delete('/semestres/{id}', [SemestreController::class, 'delete']);
 });
 
 Route::prefix('parcours')->group(function () {
